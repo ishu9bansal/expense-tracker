5a5f547183aa586e0da1a59fdba56bc9c4b9417a
Ishu Bansal <ishubansal1400@gmail.com>
2024-12-08 08:43:50 +0530

    add expenseSlice

---
+++ src/slices/expenseSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const generateNewId = (state) => {
+    let newId = -1;
+    state.forEach(ele => {
+        newId = Math.max(newId, ele.id);
+    });
+    return newId + 1;
+}
+
+const expenseSlice = createSlice({
+    name: "expense",
+    initialState: [],
+    reducers: {
+        addExpense: (state, action) => {
+            const expense = {
+                ...action.payload.expense,
+                id: generateNewId(state),
+            }
+            state.push(expense);
+        },
+        deleteExpense: (state, action) => {
+            state = state.filter(ele => ele.id === action.payload.id);
+        },
+        editExpense: (state, action) => {
+            const updatedExpense = {
+                ...action.payload.expense,
+                id: action.payload.id,
+            };
+            state = state.map(ele => ele.id === action.payload.id ? updatedExpense : ele);
+        },
+        fillExpenses: (state, action) => {
+            state = action.payload;
+        }
+    },
+});
+
+export const { addExpense, editExpense, deleteExpense, fillExpenses } = expenseSlice.actions;
+
+export default expenseSlice.reducer;
